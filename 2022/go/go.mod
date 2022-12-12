9be3fd5cc0f8def35cb7d6bee93fee591c9d4404
Hugo Hjertén <hugohjerten@gmail.com>
2022-12-12 11:54:56 +0100

    feat: solution to day 12 part 1

--- 2022/go/go.mod
+++ 2022/go/go.mod
@@ -1,3 +1,5 @@
 module 2022/go
 
 go 1.19
+
+require github.com/gammazero/deque v0.2.1
--- 2022/go/twelve/twelve.go
+++ 2022/go/twelve/twelve.go
@@ -3,103 +3,90 @@ package twelve
 import (
 	"2022/go/utils"
 	"fmt"
-	"sort"
+
+	"github.com/gammazero/deque"
 )
 
 const input = "../input/12.txt"
 
-type Path struct {
-	p     [][]string
-	steps int
+type Coord struct {
+	X int
+	Y int
 }
 
-func (p Path) AddStep(direction string, c utils.Coordinates) *Path {
-	new := p
-	new.p = nil
-	new.p = make([][]string, len(p.p))
-	for i, r := range p.p {
-		new.p[i] = make([]string, len(r))
-		copy(new.p[i], r)
-	}
-	new.p[c.X][c.Y] = direction
-	new.steps += 1
-
-	return &new
+type Location struct {
+	distance int
+	c        Coord
 }
 
-func (p Path) NewSpot(c utils.Coordinates) bool {
-	return p.p[c.X][c.Y] == "."
-}
-
-func NewPath(x int, y int) *Path {
-	p := make([][]string, x)
-	for i := 0; i < x; i++ {
-		p[i] = make([]string, y)
-		for j := 0; j < y; j++ {
-			p[i][j] = "."
-		}
-	}
-
-	return &Path{p, 0}
-}
+type empty = struct{}
 
 type HeightMap struct {
-	m [][]rune
-	e utils.Coordinates
+	m       [][]rune
+	dest    Coord
+	queue   deque.Deque[Location]
+	visited map[Coord]empty // set
 }
 
-func (hm HeightMap) elevationOk(from utils.Coordinates, to utils.Coordinates) bool {
+func (hm HeightMap) elevationOk(from Coord, to Coord) bool {
 	return hm.m[to.X][to.Y] <= hm.m[from.X][from.Y]+1
 }
 
-func (hm HeightMap) coordinateOk(c utils.Coordinates) bool {
+func (hm HeightMap) coordinateOk(c Coord) bool {
 	return (c.X >= 0 && c.Y >= 0) && (c.X < len(hm.m) && c.Y < len(hm.m[0]))
 }
 
-func (hm HeightMap) try(c utils.Coordinates, direction string, p *Path) *Path {
-	var n utils.Coordinates
-	switch direction {
-	case ">":
-		n = utils.Coordinates{X: c.X + 1, Y: c.Y}
-	case "<":
-		n = utils.Coordinates{X: c.X - 1, Y: c.Y}
-	case "v":
-		n = utils.Coordinates{X: c.X, Y: c.Y + 1}
-	case "^":
-		n = utils.Coordinates{X: c.X, Y: c.Y - 1}
+func (hm HeightMap) getNeighbours(c Coord) []Coord {
+	ns := []Coord{
+		{X: c.X + 1, Y: c.Y},
+		{X: c.X - 1, Y: c.Y},
+		{X: c.X, Y: c.Y + 1},
+		{X: c.X, Y: c.Y - 1},
 	}
 
-	if !hm.coordinateOk(n) || !hm.elevationOk(c, n) || !p.NewSpot(n) {
-		return nil
+	valid := make([]Coord, 0)
+	for _, n := range ns {
+		if hm.coordinateOk(n) && hm.elevationOk(c, n) {
+			valid = append(valid, n)
+		}
 	}
-
-	new := p.AddStep(direction, n)
-	return hm.Find(n, new)
+	return valid
 }
 
-func (hm HeightMap) Find(c utils.Coordinates, p *Path) *Path {
-	if c == hm.e {
-		p.p[c.X][c.Y] = "E"
-		return p
+func (hm *HeightMap) Find() int {
+	for {
+		if hm.queue.Len() == 0 {
+			break
+		}
+
+		loc := hm.queue.PopFront()
+		if loc.c == hm.dest {
+			return loc.distance
+		}
+
+		_, visited := hm.visited[loc.c]
+
+		if !visited {
+			hm.visited[loc.c] = empty{}
+
+			for _, n := range hm.getNeighbours(loc.c) {
+				_, visited = hm.visited[n]
+
+				if !visited {
+					hm.queue.PushBack(Location{loc.distance + 1, n})
+				}
+			}
+		}
+
 	}
 
-	paths := make([]*Path, 4)
-	paths[0] = hm.try(c, ">", p)
-	paths[1] = hm.try(c, "<", p)
-	paths[2] = hm.try(c, "v", p)
-	paths[3] = hm.try(c, "^", p)
-
-	sort.Slice(paths, func(i, j int) bool {
-		return (paths[i] == nil && paths[j] == nil) || (paths[i] == nil) || ((paths[j] != nil) && paths[i].steps < paths[j].steps)
-	})
-
-	return paths[0]
+	panic("No path could be found!")
 }
 
-func NewHeightMap(lines []string) (HeightMap, utils.Coordinates) {
+func NewHeightMap(lines []string) HeightMap {
 	m := make([][]rune, len(lines))
-	var c utils.Coordinates
-	var e utils.Coordinates
+	var s Coord
+	var e Coord
 
 	for i, l := range lines {
 		m[i] = make([]rune, len(l))
@@ -108,26 +95,31 @@ func NewHeightMap(lines []string) (HeightMap, utils.Coordinates) {
 			switch r {
 			case 83: // S
 				r = 97 // a
-				c = utils.Coordinates{X: i, Y: j}
+				s = Coord{X: i, Y: j}
 			case 69: // E
 				r = 122 // z
-				e = utils.Coordinates{X: i, Y: j}
+				e = Coord{X: i, Y: j}
 			}
 
 			m[i][j] = r
 		}
 	}
 
-	return HeightMap{m, e}, c
+	var queue deque.Deque[Location]
+	queue.PushBack(Location{0, s})
+
+	return HeightMap{
+		m:       m,
+		dest:    e,
+		queue:   queue,
+		visited: map[Coord]struct{}{},
+	}
 }
 
 func Run() {
 	lines := utils.ReadLines(input)
-	hm, start := NewHeightMap(lines)
-	p := NewPath(len(hm.m), len(hm.m[0]))
-	path := hm.Find(start, p)
-	for _, r := range path.p {
-		fmt.Println(r)
-	}
+	hm := NewHeightMap(lines)
+	shortest := hm.Find()
+	fmt.Println("Shortest: ", shortest)
 
 }
