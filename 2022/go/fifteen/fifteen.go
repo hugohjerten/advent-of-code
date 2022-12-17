8bd57857451e4c807f006c7cd4d65d3d3f262c6e
Hugo Hjertén <hugohjerten@gmail.com>
2022-12-17 10:52:34 +0100

    feat: solution to day 15 part 1

--- 2022/go/fifteen/fifteen.go
+++ 2022/go/fifteen/fifteen.go
@@ -8,6 +8,7 @@ import (
 )
 
 const input = "../input/15.txt"
+const row = 2000000
 
 type Coord struct {
 	x int
@@ -16,139 +17,9 @@ type Coord struct {
 
 type Pair = [2]Coord
 
-type Tunnels struct {
-	m       [][]rune
-	w       int
-	h       int
-	xOffset int
-	yOffset int
-	pairs   []Pair
-}
-
-func (t Tunnels) Show() {
-	for _, r := range t.m {
-		fmt.Println(string(r))
-	}
-}
-
-func (t *Tunnels) addSensorsAndBeacons(cs []Pair) {
-	for _, p := range cs {
-		sx := p[0].x - t.xOffset
-		sy := p[0].y - t.yOffset
-		bx := p[1].x - t.xOffset
-		by := p[1].y - t.yOffset
-
-		t.m[sy][sx] = 83 // "S"
-		t.m[by][bx] = 66 // "B"
-	}
-}
-
-func padding(nbr int) []rune {
-	padding := make([]rune, nbr)
-	for k := 0; k < nbr; k++ {
-		padding[k] = 46 // "."
-	}
-	return padding
-}
-
-func (t *Tunnels) expandLeft(nbr int) {
-	for i, row := range t.m {
-		t.m[i] = append(padding(nbr), row...)
-	}
-	t.w += nbr
-	t.xOffset -= nbr
-}
-
-func (t *Tunnels) expandRight(nbr int) {
-	for i, row := range t.m {
-		t.m[i] = append(row, padding(nbr)...)
-	}
-	t.w += nbr
-}
-
-func (t *Tunnels) expandUp(nbr int) {
-	pad := make([][]rune, nbr)
-	for i := 0; i < nbr; i++ {
-		pad[i] = padding(t.w)
-	}
-	t.m = append(pad, t.m...)
-	t.h += nbr
-	t.yOffset -= nbr
-}
-
-func (t *Tunnels) expandDown(nbr int) {
-	pad := make([][]rune, nbr)
-	for i := 0; i < nbr; i++ {
-		pad[i] = padding(t.w)
-	}
-	t.m = append(t.m, pad...)
-	t.h += nbr
-}
-
-func (t *Tunnels) noBeacons(c Coord, d int) {
-	if c.x-t.xOffset-d < 0 {
-		t.expandLeft(utils.Abs(c.x - t.xOffset - d))
-	}
-	if c.x-t.xOffset+d >= t.w {
-		t.expandRight((c.x - t.xOffset + d) - t.w + 1)
-	}
-	if c.y-t.yOffset-d < 0 {
-		t.expandUp(utils.Abs(c.y - t.yOffset - d))
-	}
-	if c.y-t.yOffset+d >= t.h {
-		t.expandDown((c.y - t.yOffset + d) - t.h + 1)
-	}
-	sx := c.x - t.xOffset
-	sy := c.y - t.yOffset
-
-	for y := sy - d; y <= sy+d; y++ {
-		for x := sx - d; x <= sx+d; x++ {
-			if (utils.ManhattanDistance(sx, sy, x, y) <= d) &&
-				t.m[y][x] == 46 { // "."
-
-				t.m[y][x] = 35 // "#"
-			}
-		}
-	}
-}
-
-func (t *Tunnels) EmptyPositions(row int, show bool) {
-	for _, p := range t.pairs {
-		d := utils.ManhattanDistance(p[0].x, p[0].y, p[1].x, p[1].y)
-		t.noBeacons(p[0], d)
-	}
-
-	row -= t.yOffset
-	cnt := 0
-	for _, r := range t.m[row] {
-		if r == 35 { // "#"
-			cnt += 1
-		}
-	}
-
-	if show {
-		t.Show()
-	}
-
-	fmt.Println("Empty positions: ", cnt)
-}
-
-func minMax(old [2]int, n int) [2]int {
-	if n < old[0] {
-		old[0] = n
-	}
-	if n > old[1] {
-		old[1] = n
-	}
-	return old
-}
-
-func ReadInput(input string) Tunnels {
+func ReadInput(input string) []Pair {
 	lines := utils.ReadLines(input)
 	pairs := make([][2]Coord, len(lines))
-
-	xEdges := [2]int{0, 0}
-	yEdges := [2]int{0, 0}
 
 	for i, l := range lines {
 		l = strings.ReplaceAll(l, "Sensor at x=", "")
@@ -162,32 +33,37 @@ func ReadInput(input string) Tunnels {
 		beaconX, _ := strconv.Atoi(beacon[0])
 		beaconY, _ := strconv.Atoi(beacon[1])
 		pairs[i] = [2]Coord{{sensorX, sensorY}, {beaconX, beaconY}}
-
-		xEdges = minMax(xEdges, sensorX)
-		xEdges = minMax(xEdges, beaconX)
-		yEdges = minMax(yEdges, sensorY)
-		yEdges = minMax(yEdges, beaconY)
 	}
 
-	w := xEdges[1] - xEdges[0] + 1
-	h := yEdges[1] - yEdges[0] + 1
+	return pairs
+}
 
-	m := make([][]rune, h)
-	for i := range m {
-		m[i] = make([]rune, w)
-		for j := 0; j < w; j++ {
-			m[i][j] = 46 // "."
+func noBeacons(p Pair, set map[Coord]struct{}) map[Coord]struct{} {
+	sx := p[0].x
+	sy := p[0].y
+	bx := p[1].x
+	by := p[1].y
+	d := utils.ManhattanDistance(sx, sy, bx, by)
+
+	for x := sx - d; x <= sx+d; x++ {
+		if !(x == bx && row == by) &&
+			!(x == sx && row == sy) &&
+			(utils.ManhattanDistance(sx, sy, x, row) <= d) {
+			set[Coord{x, row}] = struct{}{}
 		}
 	}
+	return set
+}
 
-	tunnels := Tunnels{m, w, h, xEdges[0], yEdges[0], pairs}
-	tunnels.addSensorsAndBeacons(pairs)
-
-	return tunnels
+func EmptyPositions(ps []Pair) {
+	set := map[Coord]struct{}{}
+	for _, p := range ps {
+		set = noBeacons(p, set)
+	}
+	fmt.Println("Number positions: ", len(set))
 }
 
 func Run() {
-	tunnels := ReadInput(input)
-	tunnels.EmptyPositions(10, true)
-
+	pairs := ReadInput(input)
+	EmptyPositions(pairs)
 }
