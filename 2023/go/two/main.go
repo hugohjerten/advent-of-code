280c22de3214b3b23a1291894d44df194660bc88
Hugo Hjertén <hugohjerten@gmail.com>
2023-12-04 13:04:10 +0100

    feat: solution to day 2 part 2

--- 2023/go/two/main.go
+++ 2023/go/two/main.go
@@ -9,7 +9,7 @@ func setsOk(red int, green int, blue int, sets []map[string]int) bool {
 	return true
 }
 
-func determineSumOfGames(red int, green int, blue int, games []Game) {
+func sumOfGames(red int, green int, blue int, games []Game) {
 	sum := 0
 	for _, g := range games {
 		if setsOk(red, green, blue, g.sets) {
@@ -19,7 +19,32 @@ func determineSumOfGames(red int, green int, blue int, games []Game) {
 	println("Part 1: ", sum)
 }
 
+func minimumPower(sets []map[string]int) int {
+	var red, green, blue int = 0, 0, 0
+	for _, s := range sets {
+		if s["red"] > red {
+			red = s["red"]
+		}
+		if s["green"] > green {
+			green = s["green"]
+		}
+		if s["blue"] > blue {
+			blue = s["blue"]
+		}
+	}
+	return red * green * blue
+}
+
+func sumOfPowerOfSets(games []Game) {
+	sum := 0
+	for _, g := range games {
+		sum += minimumPower(g.sets)
+	}
+	println("Part 2: ", sum)
+}
+
 func Run() {
 	games := parseGames()
-	determineSumOfGames(12, 13, 14, games)
+	sumOfGames(12, 13, 14, games)
+	sumOfPowerOfSets(games)
 }
