b15a7807aa3ef52b14aa16cb5c3e5841d2cb0f0d
Hugo Hjertén <hugohjerten@gmail.com>
2023-12-11 22:05:10 +0100

    feat: solution to day 6 part 1

--- twentythree/python/__main__.py
+++ twentythree/python/__main__.py
@@ -3,8 +3,9 @@ import sys
 
 from twentythree.python.five import main as five
 from twentythree.python.four import main as four
+from twentythree.python.six import main as six
 
-DAYS = {4: four.main, 5: five.main}
+DAYS = {4: four.main, 5: five.main, 6: six.main}
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:  # noqa: PLR2004
---
+++ twentythree/python/six/main.py
@@ -0,0 +1,39 @@
+"""Six."""
+from pathlib import Path
+
+from ..utils.parse import read_file
+
+path = Path(__file__).parent.parent.parent / "input" / "6.txt"
+Race = tuple[int, int]
+
+
+def parse_input() -> list[Race]:
+    """Parse input."""
+    lines = [[int(k) for k in line.split(" ")[1:] if k] for line in read_file(path)]
+    return [(lines[0][i], lines[1][i]) for i in range(len(lines[0]))]
+
+
+def get_ways_to_win(race: Race) -> int:
+    """Get number of ways to win race."""
+    ways_to_win = 0
+    for speed in range(race[0] + 1):
+        ms_left = race[0] - speed
+        if speed * ms_left > race[1]:
+            ways_to_win += 1
+    return ways_to_win
+
+
+def part_1():
+    """Part 1."""
+    lines = [[int(k) for k in line.split(" ")[1:] if k] for line in read_file(path)]
+    races = [(lines[0][i], lines[1][i]) for i in range(len(lines[0]))]
+    product = 1
+    for race in races:
+        product *= get_ways_to_win(race)
+
+    print("Part 1: ", product)
+
+
+def main():
+    """Main."""
+    part_1()
