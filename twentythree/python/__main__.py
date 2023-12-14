34c3e4d4cc3a456823a958972d403704f060080c
Hugo Hjertén <hugohjerten@gmail.com>
2023-12-14 16:39:58 +0100

    feat: solution to day 8 part 1

--- twentythree/python/__main__.py
+++ twentythree/python/__main__.py
@@ -1,12 +1,13 @@
 """Main."""
 import sys
 
+from twentythree.python.eight import main as eight
 from twentythree.python.five import main as five
 from twentythree.python.four import main as four
 from twentythree.python.seven import main as seven
 from twentythree.python.six import main as six
 
-DAYS = {4: four.main, 5: five.main, 6: six.main, 7: seven.main}
+DAYS = {4: four.main, 5: five.main, 6: six.main, 7: seven.main, 8: eight.main}
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:  # noqa: PLR2004
---
+++ twentythree/python/eight/main.py
@@ -0,0 +1,59 @@
+"""Eight."""
+from pathlib import Path
+from typing import NamedTuple
+
+from ..utils.parse import read_file
+
+# ruff: noqa: PLR2004
+
+path = Path(__file__).parent.parent.parent / "input" / "8.txt"
+
+
+class Node(NamedTuple):
+    """Node."""
+
+    left: str
+    right: str
+
+
+Network = dict[str, Node]
+Directions = list[str]
+
+
+def parse_input() -> tuple[Network, Directions]:
+    """Parse input."""
+    lines = read_file(path)
+    directions = lines[0]
+    network = {}
+    for line in lines[2:]:
+        name, left_right = line.strip(")").split(" = (")
+        network[name] = Node(*left_right.split(", "))
+
+    return network, directions
+
+
+def part_1():
+    """Part 1."""
+    network, directions = parse_input()
+
+    i = 0
+    cnt = 0
+    node = "AAA"
+    while node != "ZZZ":
+        if i == len(directions):
+            i = 0
+
+        if directions[i] == "R":
+            node = network[node].right
+        else:
+            node = network[node].left
+
+        cnt += 1
+        i += 1
+
+    print("Part 1: ", cnt)
+
+
+def main():
+    """Main."""
+    part_1()
