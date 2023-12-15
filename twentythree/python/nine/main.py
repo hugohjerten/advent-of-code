570ee247ad138da187d7c109326943fc2747f562
Hugo Hjertén <hugohjerten@gmail.com>
2023-12-15 09:35:23 +0100

    feat: solution to day 9 part 2

--- twentythree/python/nine/main.py
+++ twentythree/python/nine/main.py
@@ -14,9 +14,8 @@ def parse_input() -> list[History]:
     return [[int(val) for val in line.split()] for line in read_file(path)]
 
 
-def _extrapolate(history: History) -> int:
-    """Extrapolate next value."""
-    # Find next sequences
+def _find_next_sequences(history: History) -> list[History]:
+    """Find next sequences."""
     seqs = [history]
     while True:
         seqs.append([])
@@ -25,6 +24,14 @@ def _extrapolate(history: History) -> int:
 
         if all(val == 0 for val in seqs[-1]):
             break
+
+    return seqs
+
+
+def _extrapolate_right(history: History) -> int:
+    """Extrapolate next value."""
+    # Find next sequences
+    seqs = _find_next_sequences(history)
 
     # Append zero at end of last sequence
     seqs[-1].append(0)
@@ -36,13 +43,36 @@ def _extrapolate(history: History) -> int:
     return seqs[0][-1]
 
 
+def _extrapolate_left(history: History) -> int:
+    """Extrapolate next value."""
+    # Find next sequences
+    seqs = _find_next_sequences(history)
+
+    # Add zero at beginning of last sequence
+    seqs[-1].insert(0, 0)
+
+    # Extrapolate value for each sequence
+    for i in range(len(seqs) - 1)[::-1]:
+        seqs[i].insert(0, seqs[i][0] - seqs[i + 1][0])
+
+    return seqs[0][0]
+
+
 def part_1():
     """Part 1."""
     histories = parse_input()
-    extrapolated = [_extrapolate(h) for h in histories]
+    extrapolated = [_extrapolate_right(h) for h in histories]
     print("Part 1: ", sum(extrapolated))
+
+
+def part_2():
+    """Part 2."""
+    histories = parse_input()
+    extrapolated = [_extrapolate_left(h) for h in histories]
+    print("Part 2: ", sum(extrapolated))
 
 
 def main():
     """Main."""
     part_1()
+    part_2()
