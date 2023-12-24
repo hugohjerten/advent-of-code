4b62c1f668a01bb1a1aaaf9827a7ee950c3bf713
Hugo Hjertén <hugohjerten@gmail.com>
2023-12-24 13:04:34 +0100

    chore: rm last empty last line from input

--- twentythree/python/utils/parse.py
+++ twentythree/python/utils/parse.py
@@ -4,4 +4,7 @@ from pathlib import Path
 
 def read_file(path: Path) -> list[str]:
     """Read file."""
-    return [line for line in path.read_text(encoding="utf-8").splitlines()]
+    lines = [line for line in path.read_text(encoding="utf-8").splitlines()]
+    if lines[-1] == "":
+        lines = lines[:-1]
+    return lines
