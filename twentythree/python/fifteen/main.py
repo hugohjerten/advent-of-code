66416dd592a39c09f015787351096b700a6ce992
Hugo Hjertén <hugohjerten@gmail.com>
2023-12-28 11:59:49 +0100

    feat: solution to day 15 part 2

--- twentythree/python/fifteen/main.py
+++ twentythree/python/fifteen/main.py
@@ -5,6 +5,11 @@ from ..utils.parse import read_file
 
 path = Path(__file__).parent.parent.parent / "input" / "15.txt"
 
+# Label, focal length
+Lens = tuple[str, int]
+# key: box number, value: list of lenses
+Boxes = dict[int, list[Lens]]
+
 
 def parse_input() -> list[str]:
     """Parse input."""
@@ -30,6 +35,62 @@ def part_1():
     print("Part 1: ", total)
 
 
+def _remove_lens(label: str, box: int, boxes: Boxes) -> Boxes:
+    """Remove lens."""
+    if box not in boxes:
+        return boxes
+
+    for i, lens in enumerate(boxes[box]):
+        if label == lens[0]:
+            boxes[box].pop(i)
+
+            if not boxes[box]:
+                del boxes[box]
+
+            return boxes
+
+    return boxes
+
+
+def _add_lens(new: Lens, box: int, boxes: Boxes) -> Boxes:
+    """Add lens."""
+    if box not in boxes:
+        boxes[box] = [new]
+        return boxes
+
+    for i, lens in enumerate(boxes[box]):
+        if new[0] == lens[0]:
+            boxes[box][i] = new
+            return boxes
+
+    boxes[box].append(new)
+    return boxes
+
+
+def part_2():
+    """Part 2."""
+    sequence = parse_input()
+
+    boxes: Boxes = {}
+    for s in sequence:
+        label = s.split("-")[0] if "-" in s else s.split("=")[0]
+        box = hash_algorithm(label)
+
+        if "-" in s:
+            boxes = _remove_lens(label, box, boxes)
+        elif "=" in s:
+            boxes = _add_lens(Lens((label, int(s[-1]))), box, boxes)
+
+    total = sum(
+        [
+            sum([(box + 1) * (i + 1) * lens[1] for i, lens in enumerate(lenses)])
+            for box, lenses in boxes.items()
+        ]
+    )
+    print("Part 2:", total)
+
+
 def main():
     """Main."""
     part_1()
+    part_2()
